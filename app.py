00dbf43cf10a2160446faf1e67e4fbe33a38a706
Raja Singh Thakur <raja1011singhrajput@gmail.com>
2025-01-18 20:10:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pickle
+import pandas as pd
+
+def recommend(movie):
+    movie_index = movies[movies['Movie Name'].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)),reverse = True, key = lambda x: x[1])[1:6]
+
+    for i in movie_list:
+        
+
+
+st.set_confi('netflix Recommender System')
 
 # Set up the title for the app
 st.title('Netflix Recommender System')
 
 # Load the movie list from the pickle file
 movie_list = pickle.load(open('movie_list.pkl', 'rb'))
+movies = pd.Dataframe(movie_list)
 
 # Extract movie names from the dataframe
 movie_list = movie_list['Movie Name'].values
@@ -17,7 +30,8 @@ option = st.selectbox(
 )
 
 # Display the selected movie
-st.write(f'You selected: {option}')
+if st.button('Recommend')
+   st.write(f'You selected: {option}')
 
 # Implement your recommendation logic here
 # Example: assuming you have a recommendation function `recommend()` that uses the selected movie
